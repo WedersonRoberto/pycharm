03d7691510d5a9307e861b2ee98d7389e671f5c4
wederson <emrferramentas@gmail.com>
2026-05-12 14:35:45 -0300

    salvar

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+import sys
+import os
+import platform
+
+def shutdowmn():
+    sistema = platform.system().lower()
+    try:
+        if "windows"in sistema :
+            os.system("shutdown /s /t 0")
+        elif "linux"in sistema or "darwin" in sistema :
+            os.system("shutdown now")
+        else:
+            print("\nSistema operacional não reconhecido")
+    except Exception as e :
+        print(f"Erro ao tentar o shutdown : {e}")
+
+def temporizador_com_shutdown():
+    try:
+        entrada = input("\nQuantos segundos até o desligamento? ")
+        segundos = int(entrada)
+
+        while segundos >0 :
+            mins, segundos = divmod(segundos, 60)
+            timer = f"{mins:02d}:{segundos:02d}"
+
+            # Bip nos 10 segundos finais
+            bip = "\a" if 0 < segundos < 10 else ""
+            print(f"\r Tempo restante :{timer}{bip} ", end="" , flush=True)
+            time.sleep(1)
+            segundos -= 1
+            print("\n\nIniciando o desligamento... Tchau🙋‍♂️")
+            shutdowmn()
+    except ValueError :
+        print("\Erro : Por favor , digite apenas números inteiros")
+    except KeyboardInterrupt :
+        print("\n\nOperação cancelada pelo usuário")
+if __name__ == "__main__" :
+    temporizador_com_shutdown()
